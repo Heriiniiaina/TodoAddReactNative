d604d0de8e809d02e9c29e3533a198953a642f93
Heriniaina <heriniainaramamonjitiana@gmail.com>
2024-11-02 19:24:33 +0100

    az

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import Card from '@/components/Card/Card';
 import Header from '@/components/Header';
+import TabMenu from '@/components/TabMenu/TabMenu';
 import { styles } from '@/constants/App.style';
 import React, { useState } from 'react';
 import { Image, StyleSheet, Platform, View, Text, ScrollView } from 'react-native';
@@ -7,43 +8,57 @@ import { Image, StyleSheet, Platform, View, Text, ScrollView } from 'react-nativ
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context"
 
 export default function HomeScreen() {
-type Todo = {
-    id:number,
-    title:string,
-    isCompleted:boolean
+  type Todo = {
+    id: number,
+    title: string,
+    isCompleted: boolean
 
   }
-  const [todoList,setTodoLIst] = useState<Todo[]>([
-    { id:1,title:"Mangery",isCompleted:true  },
-    { id:2,title:"Mamany",isCompleted:false  },
-    { id:3,title:"Misasa",isCompleted:true  },
-    { id:4,title:"Micode",isCompleted:true  },
-    { id:5,title:"Manakorotana",isCompleted:false  },
-    { id:6,title:"Migafy",isCompleted:false  },
-    { id:7,title:"Mijery boruto",isCompleted:true  },
-    { id:8,title:"Misakafo",isCompleted:true  },
+  const [selectMenu, setMenuSelect] = useState<string>("all")
+
+  const [todoList, setTodoLIst] = useState<Todo[]>([
+    { id: 1, title: "Mangery", isCompleted: true },
+    { id: 2, title: "Mamany", isCompleted: false },
+    { id: 3, title: "Misasa", isCompleted: true },
+    { id: 4, title: "Micode", isCompleted: true },
+    { id: 5, title: "Manakorotana", isCompleted: false },
+    { id: 6, title: "Migafy", isCompleted: false },
+    { id: 7, title: "Mijery boruto", isCompleted: true },
+    { id: 8, title: "Misakafo", isCompleted: true },
   ])
- 
-  const updateTodo = (todo:Todo)=>{
-      const updatedTodo = {
-        ...todo,
-        isCompleted: !todo.isCompleted
+
+  const getFilteredList =()=>{
+      switch(selectMenu){
+        case"all":
+          return todoList
+        case "inProgress":
+          return todoList.filter(todo=> !todo.isCompleted)
+        case "done":
+          return todoList.filter(todo=> todo.isCompleted)
+        default :return []
       }
-      const todoIndex = todoList.findIndex((elm)=>elm.id == updatedTodo.id)
-      const updatedList = [...todoList]
-      updatedList[todoIndex] = updatedTodo
-      setTodoLIst(updatedList)
   }
-  const renderTodo = ()=>{
-    return todoList.map((todo)=><View key={todo.id} style={styles.cardTodo}><Card onPress={updateTodo} todo={todo}/></View>)
- }
+
+  const updateTodo = (todo: Todo) => {
+    const updatedTodo = {
+      ...todo,
+      isCompleted: !todo.isCompleted
+    }
+    const todoIndex = todoList.findIndex((elm) => elm.id == updatedTodo.id)
+    const updatedList = [...todoList]
+    updatedList[todoIndex] = updatedTodo
+    setTodoLIst(updatedList)
+  }
+  const renderTodo = () => {
+    return getFilteredList().map((todo) => <View key={todo.id} style={styles.cardTodo}><Card onPress={updateTodo} todo={todo} /></View>)
+  }
   return (
     <>
       <SafeAreaProvider >
 
         <SafeAreaView style={styles.app}>
           <View style={styles.header}>
-              <Header/>
+            <Header />
           </View>
           <View style={styles.body}>
             <ScrollView>
@@ -51,12 +66,12 @@ type Todo = {
               {renderTodo()}
             </ScrollView>
           </View>
-          
+
         </SafeAreaView>
       </SafeAreaProvider>
       <View style={styles.footer}>
-            <Text>Footer</Text>
-          </View>
+        <TabMenu onPress={setMenuSelect} selectMenu={selectMenu} todoList={todoList} />
+      </View>
     </>
   );
 }
--- components/TabMenu/TabMenu.tsx
+++ components/TabMenu/TabMenu.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, TouchableOpacity, View } from 'react-native'
+import { s } from './TabMenu.style'
 
-
-
-const TabMenu = () => {
+type Todo ={
+    id:number,
+    title:string,
+    isCompleted:boolean
+}
+interface SelectProps {
+    selectMenu:string,
+    onPress:(menu:string)=>void,
+    todoList: Todo[]
+}
+const TabMenu = ({selectMenu,onPress,todoList }:SelectProps) => {
+    const countByStatus = (Array.isArray(todoList) ? todoList : []).reduce((acc,todo)=>{
+        todo.isCompleted ? acc.done ++ : acc.inProgress++
+        return acc
+    },{all:todoList ? todoList.length : 0,inProgress:0,done:0})
+    const getStyle = (tabName: string) => {
+          return {
+            fontWeight:  'bold' as "bold",
+            color: tabName === selectMenu ? '#2F76E5' : 'black',
+          };
+        };
+    console.log(countByStatus)
   return (
-    <View>
-        <Text>Hello</Text>
+    <View style={s.container}>
+        <TouchableOpacity onPress={()=>onPress("all")}><Text style={getStyle("all")}>All ({countByStatus.all})</Text></TouchableOpacity>
+        <TouchableOpacity onPress={()=>onPress("inProgress")}><Text style={getStyle("inProgress")}>In progress ({countByStatus.inProgress})</Text></TouchableOpacity>
+        <TouchableOpacity onPress={()=>onPress("done")}><Text style={getStyle("done")}>Done ({countByStatus.done})</Text></TouchableOpacity>
     </View>
   )
 }
